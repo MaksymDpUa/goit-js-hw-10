e56e1f471000f4d43ea0b77baba0591781971e53
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-04-20 19:06:30 +0200

    created function fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
+import { fetchCountries } from "./helpers/fetchCountries.js"
+
 
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('country-list');
+const countryInfo = document.querySelector('country-info');
+
+searchBox.addEventListener('input', onSearch);
+
+function onSearch() {
+
+}
+
+
+fetchCountries('deutschland').then(data => console.log(data))
