d0e88ef73a549559546d33314c936201fcbfd627
Maksym Ponomarenko <ponomarenko2983@gmail.com>
2023-04-20 19:15:20 +0200

    add debounce

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import './css/styles.css';
 import { fetchCountries } from "./helpers/fetchCountries.js"
 
-
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('#search-box');
 const countryList = document.querySelector('country-list');
 const countryInfo = document.querySelector('country-info');
 
-searchBox.addEventListener('input', onSearch);
+searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-function onSearch() {
-
+function onSearch(evt) {
+    const country = evt.target.value;
+fetchCountries(country).then(data => console.log(data))
+    console.log(country);
 }
 
 
-fetchCountries('deutschland').then(data => console.log(data))
+// fetchCountries('deutschland').then(data => console.log(data))
